92412f6171f2bf39008feb3b0448ea567e9caada
CalebJ <70595794+S11031432@users.noreply.github.com>
2021-11-30 14:35:52 -0500

    v0.4
    Added color names and color values.

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First Pygame, Caleb Jones, 11/30/21, 2:24PM, v0.2
+# My First Pygame, Caleb Jones, 11/30/21, 2:35PM, v0.4
 
 import pygame, sys
 from pygame.locals import *
@@ -9,4 +9,16 @@ pygame.init()
 # Setup the game window.
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Hello, world!')
+
+# Setup color values.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLOODBLUE = (255, 0, 255)
+
+
+
+
 
