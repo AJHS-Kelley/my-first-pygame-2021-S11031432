721ac5cbaad10a616ce348dc2e23cfe7589b65ed
CalebJ <70595794+S11031432@users.noreply.github.com>
2022-01-04 14:27:12 -0500

    v.0.1
    Imported the pygame, system, and random modules.
    
    Setup PyGame and created mainClock variable.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,8 @@
-# Simple Animation with PyGame, Caleb Jones, 1/4/22, 2:19PM, v.0.0
+# Simple Animation with PyGame, Caleb Jones, 1/4/22, 2:24PM, v.0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+mainClock = pygame.time.Clock()
