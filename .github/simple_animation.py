11831ce4832ba0769d1f5d26bf385de87b6d85ac
CalebJ <70595794+S11031432@users.noreply.github.com>
2022-01-04 14:39:17 -0500

    v.0.1
    Imported modules for PyGame System, and Time.
    
    Initialized the PyGame Module.
    
    Setup window dimensions.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,8 +1,20 @@
-# Simple Animation with PyGame, Caleb Jones, 1/4/22, 2:24PM, v.0.1
+# Simple Animation with PyGame, Caleb Jones, 1/4/22, 2:36PM, v.0.1
 
 import pygame, sys, random
 from pygame.locals import *
 
 # Setup PyGame
 pygame.init()
-mainClock = pygame.time.Clock()
+
+# Setup the Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
+
+
+
+
+
+
+
