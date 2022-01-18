ec968aa14c632783780c87b3aec1482b8565fd2d
CalebJ <70595794+S11031432@users.noreply.github.com>
2022-01-18 13:51:28 -0500

    v.0.1

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,1 +1,8 @@
-# PyGame Collision Detection Practice, Caleb Jones, January 18, 2022, 1:43pm, v.0.0
+# PyGame Collision Detection Practice, Caleb Jones, January 18, 2022, 1:51pm, v.0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+mainClock = pygame.time.Clock()
